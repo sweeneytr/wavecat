f22a84324c8a494e8f68824b8f48e004b9b6aa92
Tristan Sweeney <sweeneytri@gmail.com>
2023-07-07 00:18:51 -0400

    Fix secret name

--- foundry-server/foundry/build_secrets.sh
+++ foundry-server/foundry/build_secrets.sh
@@ -1,6 +1,6 @@
 #! /bin/sh 
 kubectl create secret generic \
-    route53-credentials-secret \
+    foundry-secrets \
     --from-file=./foundry-secrets.secret \
     --dry-run=client -o json \
 | kubeseal \
