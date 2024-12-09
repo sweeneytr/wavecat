8bb8b9c2f12756e109247ad73233cb6b57a41419
Tristan Sweeney <sweeneytri@gmail.com>
2024-12-09 13:57:48 -0500

    pls

--- gamepad-server/build_secrets.sh
+++ gamepad-server/build_secrets.sh
@@ -5,6 +5,11 @@ if [ -z "$1" ]
     echo "No argument supplied"
 fi
 
+if [ -z "$2" ]
+  then
+    echo "No argument supplied"
+fi
+
 cat << EOF > ghcr.secret
 {
     "auths":
@@ -17,10 +22,12 @@ cat << EOF > ghcr.secret
 }
 EOF
 
-kubectl create secret generic \
-    ghcr-secret \
-    --from-file=dockerconfig.json=./ghcr.secret \
-    --dry-run=client -o json \
-| kubeseal \
+kubectl create secret docker-registry \
+    ghcr-secret --docker-server=https://ghcr.io \
+    --namespace gamepad-server \
+    --docker-server=https://ghcr.io \
+    --docker-username="$1" \
+    --docker-password="$2" \
+    --dry-run=client -o json | kubeseal \
     --namespace gamepad-server \
     > ghcr-secret.json
