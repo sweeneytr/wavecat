9fd0781b4ffd05bedb83f7047c581ff6b3abca0b
Tristan Sweeney <sweeneytri@gmail.com>
2024-12-09 12:42:23 -0500

    Setup ghcr secret

---
+++ infra/ghcr/build_secrets.sh
@@ -0,0 +1,25 @@
+#! /bin/sh 
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+fi
+
+cat << EOF > ghcr.secret
+{
+    "auths":
+    {
+        "ghcr.io":
+            {
+                "auth":"$(echo $1 | base64)"
+            }
+    }
+}
+EOF
+
+kubectl create secret generic \
+    ghcr-secret \
+    --from-file=.dockerconfigjson=./ghcr.secret \
+    --dry-run=client -o json \
+| kubeseal \
+    > ghcr-secret.json
